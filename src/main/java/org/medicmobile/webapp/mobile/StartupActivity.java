ac0388d48188fc3bd20aa1730acca4120f79f05b
Auston Ajith <austonajith.e@gmail.com>
2020-11-09 13:51:47 +0530

    Feature : Gis App Dependency, Flavour added
    	- Gis App Dependency added
    	- Supervisor flavour added for kenya, uganda and training

--- src/main/java/org/medicmobile/webapp/mobile/StartupActivity.java
+++ src/main/java/org/medicmobile/webapp/mobile/StartupActivity.java
@@ -1,7 +1,10 @@
 package org.medicmobile.webapp.mobile;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import static org.medicmobile.webapp.mobile.BuildConfig.DEBUG;
@@ -24,7 +27,24 @@ public class StartupActivity extends Activity {
 	private void configureAndStartNextActivity() {
 		configureHttpUseragent();
 
-		if(hasEnoughFreeSpace()) startAppActivityChain(this);
+		if(hasEnoughFreeSpace()) {
+			if(isGpsAppInstalled("com.lg.gis")) {
+				startAppActivityChain(this);
+			}else {
+				new AlertDialog.Builder(this)
+						.setTitle("GIS app is not installed")
+						.setMessage("Please install GIS app ")
+						.setCancelable(false)
+						.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								startAppActivityChain(StartupActivity.this);
+								dialog.dismiss();
+							}
+						})
+						.setIcon(R.mipmap.ic_launcher)
+						.show();
+			}
+		}
 		else {
 			Intent i = new Intent(this, FreeSpaceWarningActivity.class);
 			startActivity(i);
@@ -41,5 +61,15 @@ public class StartupActivity extends Activity {
 	private void configureHttpUseragent() {
 		String current = System.getProperty("http.agent");
 		System.setProperty("http.agent", createUseragentFrom(current));
+	}
+
+	private boolean isGpsAppInstalled(String packageName) {
+		try {
+			PackageManager pm = this.getPackageManager();
+			pm.getPackageInfo(packageName, 0);
+			return true;
+		} catch (PackageManager.NameNotFoundException e) {
+			return false;
+		}
 	}
 }
